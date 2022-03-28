e7db234fb9cccb0f4c24b7baea8ee4990119e665
Nikita Shvetsov <nshvtsv@gmail.com>
2022-03-28 06:00:22 -0700

    готовый слайдер

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,26 @@
-let l;
+const $jq = jQuery.noConflict();
+
+$jq(document).ready(function () {
+  $jq(".slider-freelance").slick({
+    // mobileFirst: true,
+    variableWidth: true,
+    centerMode: true,
+    arrows: true,
+    dots: false,
+    slidesToShow: 3,
+    autoplay: false,
+    infinite: true,
+    speed: 1800,
+    // adaptiveHeight: true,
+    pauseOnFocus: true,
+    pauseOnHover: true,
+    pauseOnDotsHover: true,
+    easing: "ease",
+    draggable: true,
+    // swipe: true,
+    touchTreshold: 20,
+    touchMove: false,
+    prevArrow: document.querySelector("#prevArrow"),
+    nextArrow: document.querySelector("#nextArrow"),
+  });
+});
--- gulp/gulp.task.distToBackend.js
+++ gulp/gulp.task.distToBackend.js
@@ -1,22 +1,18 @@
-const gulp = require('gulp');
-const gulpif = require('gulp-if');
-const browserSync = require('browser-sync').create();
+// const gulp = require('gulp');
+// const gulpif = require('gulp-if');
+// const browserSync = require('browser-sync').create();
 
-const isDev = (process.argv.indexOf('--dev') !== -1);
-const isProd = !isDev;
-const isSync = (process.argv.indexOf('--sync') !== -1);
+// const isDev = (process.argv.indexOf('--dev') !== -1);
+// const isProd = !isDev;
+// const isSync = (process.argv.indexOf('--sync') !== -1);
 
-const path = require('../path.json');
+// const path = require('../path.json');
 
-
-
-// ===============================================
-// Tasks dist all files to backend
-// ===============================================
-gulp.task('dist', function () {
-    return gulp.src(path.dist + '**/*')
-        .pipe(gulp.dest(path.backend))
-        .pipe(browserSync.stream());
-})
-
-
+// // ===============================================
+// // Tasks dist all files to backend
+// // ===============================================
+// gulp.task('dist', function () {
+//     return gulp.src(path.dist + '**/*')
+//         .pipe(gulp.dest(path.backend))
+//         .pipe(browserSync.stream());
+// })
