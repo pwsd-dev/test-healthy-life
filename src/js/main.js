c734898ef27d7a47270aef1de4d3c2b977161372
Nikita Shvetsov <nshvtsv@gmail.com>
2022-03-29 05:39:47 -0700

    done

--- src/js/main.js
+++ src/js/main.js
@@ -24,3 +24,26 @@ $jq(document).ready(function () {
     nextArrow: document.querySelector("#nextArrow"),
   });
 });
+
+function openBlock() {
+  const burgerBtn = document.getElementById("burgerBtn");
+  const otherId = document.getElementById("other");
+  burgerBtn.classList.toggle("active");
+
+  otherId.classList.toggle("hidden");
+  otherId.querySelector(".inner").classList.toggle("active");
+}
+
+$jq(document).ready(function () {
+  $jq("#burgerBtn").click(function () {
+    if ($jq("#other").hasClass("active")) {
+      $jq("#other").fadeOut();
+      $jq("body").removeClass("modal-open");
+      $jq("#other").toggleClass("active");
+    } else {
+      $jq("#other").fadeIn();
+      $jq("body").addClass("modal-open");
+      $jq("#other").toggleClass("active");
+    }
+  });
+});
